e7bb8e5168b54f090c1ddec3f1d7cc64cb9870eb
Tianika <tatyriana@yandex.ru>
2021-11-01 21:37:46 +0300

    feat: ready task 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+const fsPromises = require('fs/promises')
+const path = require('path')
+
+const pathToFolder = path.join(__dirname, 'secret-folder')
+
+fsPromises
+  .readdir(pathToFolder, { withFileTypes: true }, () => {})
+  .then((files) => {
+    for (let file of files) {
+      if (file.isFile()) {
+        let name = file.name.split('.')
+        fs.stat(path.join(pathToFolder, file.name), (err, stats) => {
+          console.log(`${name[0]} - ${name[1]} - ${stats.size / 1024} Kb`)
+        })
+      }
+    }
+  })
