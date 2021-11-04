dbf1bf51f8f9b7d751ae8450957bc69f33fcbf4f
Tianika <tatyriana@yandex.ru>
2021-11-04 12:26:58 +0300

    feat: ready task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,35 @@
+const fs = require('fs')
+const fsPromises = require('fs/promises')
+const path = require('path')
 
+const pathToOldFolder = path.join(__dirname, 'files')
+const pathToNewFolder = path.join(__dirname, 'files-copy')
+
+const files = []
+
+fs.mkdir(pathToNewFolder, { recursive: true }, (err) => {
+  if (err) console.log(err)
+})
+
+fsPromises
+  .readdir(pathToOldFolder, { withFileTypes: true }, () => {})
+  .then((datas) => {
+    for (let data of datas) {
+      fsPromises.copyFile(
+        path.join(pathToOldFolder, data.name),
+        path.join(pathToNewFolder, data.name)
+      )
+      files.push(data.name)
+    }
+  })
+  .then(() => {
+    fsPromises
+      .readdir(pathToNewFolder, { withFileTypes: true }, () => {})
+      .then((datas) => {
+        datas.forEach((data) => {
+          if (!files.includes(data.name)) {
+            fsPromises.rm(path.join(pathToNewFolder, data.name))
+          }
+        })
+      })
+  })
